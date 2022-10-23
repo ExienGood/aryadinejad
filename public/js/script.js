111e5aa0bec3c5e5a30bb2a1b558a354bed2f4cb
Arya <aryadinejad3@gmail.com>
2022-10-23 14:01:33 +0700

    add dark mode

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,33 @@ hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// DarkMode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+  if (darkToggle.checked) {
+    html.classList.add('dark')
+    localStorage.the = 'dark';
+  } else {
+    html.classList.remove('dark')
+    localStorage.the = 'light';
+  }
+});
+
+// TOGGLE SESUAI MODE
+if (localStorage.the === 'dark' || (!('the' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+  darkToggle.checked = true;
+} else {
+  document.documentElement.classList.remove('dark')
+  darkToggle.checked = false;
+}
